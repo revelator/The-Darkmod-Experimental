759103f3a25c2a628def7a7050e957a3b0ccd68b
ralph engels <ralphengels@gmail.com>
2014-11-22 17:11:23 +0100

    Customized linuxBuild.sh to help on building darkmod on updated kubuntu.
    Signed-off-by: ralph engels <ralphengels@gmail.com>

--- src/linuxBuild.sh
+++ src/linuxBuild.sh
@@ -8,6 +8,10 @@ touch scons.signatures.dblite
 # FAST=true
 # --debug=explain
 
+# move the system zlib.h header out of the place so we can use darkmods
+mv -f /usr/include/zlib.h /usr/include/zlib.h.bak
+cp -pf `pwd`/include/zlib/zlib.h /usr/include/zlib.h
+
 # $@ = pass along the flags like "BUILD=profile" or "BUILD=debug"
 time scons -j2 BUILD_GAMEPAK=1 NO_GCH=0 BUILD=release --debug=explain "$@"
 mv gamex86-base.so gamex86.so
@@ -21,4 +25,6 @@ cp tdm_game02.pk4 ~/games/tdm/darkmod/tdm_game02.pk4
 if [ -f ~/games/tdm/darkmod/gamex86.so ]; then
   rm ~/games/tdm/darkmod/gamex86.so
 fi
-
+# move zlib.h back to the original
+rm -f /usr/include/zlib.h
+mv -f /usr/include/zlib.h.bak /usr/include/zlib.h
